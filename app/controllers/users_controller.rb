97a0658f15342db412fd59aa255065b041b6244e
Andy Henson <andrew.henson@foxsoft.net>
2013-09-17 23:08:16 +0100

    Convert controller to strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    respond_with User.create(params[:user])
+    respond_with User.create(user_params)
+  end
   end
 
   def authenticate
@@ -20,4 +21,8 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
